b340f8a29393ea809151719cee8afb4b4a27201d
Emily Coco <Emily.Coco@rallyhealth.com>
2015-08-29 13:32:14 -0700

    Initial Commit

---
+++ Out The DoorTests/Out_The_DoorTests.m
@@ -0,0 +1,40 @@
+//
+//  Out_The_DoorTests.m
+//  Out The DoorTests
+//
+//  Created by Emily Coco on 8/29/15.
+//  Copyright (c) 2015 Emily Coco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Out_The_DoorTests : XCTestCase
+
+@end
+
+@implementation Out_The_DoorTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
